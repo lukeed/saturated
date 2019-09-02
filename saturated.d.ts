15a27cb54e4674b0bfa1ea73c9459d915dc375b0
Luke Edwards <luke.edwards05@gmail.com>
2019-09-01 22:21:57 -0700

    feat: update types

--- saturated.d.ts
+++ saturated.d.ts
@@ -1,3 +1,18 @@
-declare function saturated(): void;
+export interface IOptions {
+	interval?: number;
+	max?: number;
+}
+
+export type IPublisher<T = any> = (value: T) => number;
+export type ISubscriber<T = any> = (queue: T[]) => any;
+
+export interface ISaturated<T = any> {
+	push: IPublisher<T>;
+	flush: () => void;
+	reset: (toFlush?: boolean) => void;
+	size: () => number;
+}
+
+declare function saturated<T = any>(caller: ISubscriber<T>, opts?: IOptions): Readonly<ISaturated<T>>;
 
 export default saturated;
