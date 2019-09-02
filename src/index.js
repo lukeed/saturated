2bce137024452d32621c075438de36724eda824b
Luke Edwards <luke.edwards05@gmail.com>
2019-09-01 22:05:10 -0700

    feat: module logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
-export default function () {
-	//
+export default function (caller, opts) {
+	opts = opts || {};
+
+	var timer, tmp, queue = [];
+	var max = opts.max || Infinity;
+
+	function batch() {
+		clearInterval(timer);
+		caller(queue.splice(0, max));
+		ticker();
+	}
+
+	function ticker() {
+		timer = setInterval(batch, opts.interval || 10e3);
+	}
+
+	ticker();
+
+	return {
+		flush: batch,
+
+		push: function () {
+			tmp = queue.push(arguments);
+			if (tmp >= max) batch();
+			return tmp;
+		},
+
+		size: function () {
+			return queue.length;
+		},
+
+		reset: function (toFlush) {
+			if (toFlush) batch();
+			clearInterval(timer);
+		}
+	};
 }
